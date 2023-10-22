f31192fd9e9f14b1cebad9fa79ec94d8747e1b13
atsuyaw <watanabe@osakafu-u.net>
2023-10-22 16:36:40 +0900

    mod switch logo and its behavier

--- v4/assets/js/dark-toggle.js
+++ v4/assets/js/dark-toggle.js
@@ -1,11 +1,12 @@
 const dkToggle = document.getElementById("darkToggleLogo");
 const darkTheme = document.getElementById("darkTheme");
+const PCS = window.matchMedia("(prefers-color-scheme: dark)");
 
 dkToggle.addEventListener("click", () => {
-  if (dkToggle.className === "fa fa-moon") {
+  if (dkToggle.className === "fa fa-sun") {
     setTheme("dark");
   }
-  else if (dkToggle.className === "fa fa-sun") {
+  else {
     setTheme("light");
   }
 });
@@ -13,9 +14,22 @@ dkToggle.addEventListener("click", () => {
 function setTheme(mode) {
   if (mode === "dark") {
     darkTheme.disabled = false;
-    dkToggle.className = "fa fa-sun";
-  } else if (mode === "light") {
-    darkTheme.disabled = true;
     dkToggle.className = "fa fa-moon";
+  } else {
+    darkTheme.disabled = true;
+    dkToggle.className = "fa fa-sun";
   }
 }
+
+const listener = (event) => {
+  if (event.matches) {
+    setTheme("dark");
+  }
+  else {
+    setTheme("light");
+  }
+};
+
+// PCS.addEventListerner("change", listener);
+listener(PCS);
+
