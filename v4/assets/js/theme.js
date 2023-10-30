8a6d61ab1fd6953bc3ab54b60c6cdaa0500ec360
atsuyaw <watanabe@osakafu-u.net>
2023-10-30 19:35:12 +0900

    fix: fetching scripts for dark mode in theme.js

--- v4/assets/js/theme.js
+++ v4/assets/js/theme.js
@@ -95,4 +95,36 @@ $(document).ready(function () {
         $('html, body').animate({scrollTop:0}, '300');
     });
 
+  // Light dark theme mode switcher
+  const lightDarkToggle = document.getElementById("light-dark-toggle");
+  const darkThemeCss = document.getElementById("dark-theme-css");
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  lightDarkToggle.addEventListener("click", () => {
+    if (lightDarkToggle.className === "fa fa-sun") {
+      setThemeMode("dark");
+    } else {
+      setThemeMode("light");
+    }
+  });
+
+  function setThemeMode(mode) {
+    if (mode === "dark") {
+      darkThemeCss.disabled = false;
+      lightDarkToggle.className = "fa fa-moon";
+    } else {
+      darkThemeCss.disabled = true;
+      lightDarkToggle.className = "fa fa-sun";
+    }
+  }
+
+  const listener = (event) => {
+    if (event.matches) {
+      setThemeMode("dark");
+    } else {
+      setThemeMode("light");
+    }
+  };
+
+  listener(prefersDark);
 });
